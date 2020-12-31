d5c0a89322c9f8b01a41172d61da9f2385d81778
Eric Shen <ericshenyuhao@outlook.com>
2020-12-31 10:52:46 +0800

    output info logfile

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ const os = require('os');
 const log4js = require('log4js');
 const logger = log4js.getLogger();
 
-logger.level = 'info'
+logger.level = 'info';
 
 logger.info("App starting...");
+
+log4js.configure({
+    appenders: {
+      app: { type: 'file', filename: 'app.log' }
+    },
+    categories: {
+      default: { appenders: [ 'app' ], level: 'info' }
+    }
+  });
 
 var handler = function(request, response) {
   logger.info("Received request from " + request.connection.remoteAddress);
